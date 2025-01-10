16dc1f6f1648046f3608637105abc6a8847e5ac5
Ryan Tan <ryantanzr.work@gmail.com>
2025-01-11 00:57:58 +0800

    Added enriched metrics table (incomplete)

--- dags/dag.py
+++ dags/dag.py
@@ -8,6 +8,7 @@ from airflow import DAG
 from airflow.decorators import task, dag, task_group
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 import pandas as pd
+import numpy as np
 import requests
 
 default_args = {
@@ -44,7 +45,15 @@ def fetch_and_store_amazon_books_etl():
             book_type VARCHAR(255),
             rating FLOAT,
             rating_count INT
-        )
+        );
+
+        CREATE TABLE IF NOT EXISTS enriched_metrics (
+            author VARCHAR(255),
+            avg_rating FLOAT,
+            avg_price FLOAT,
+            avg_paperback_price FLOAT,
+            avg_hardcover_price FLOAT
+        );
         """
         cursor.execute(create_table_query)
         connection.commit()
@@ -113,7 +122,7 @@ def fetch_and_store_amazon_books_etl():
 
         # Perform data cleaning and standardisation before transformations
         @task(task_id="standardise_book_data")
-        def standardise_book_data(raw_books: pd.DataFrame):
+        def standardise_book_data(raw_books: pd.DataFrame) -> pd.DataFrame:
 
             # check data validity
             if raw_books.empty:
@@ -143,23 +152,65 @@ def fetch_and_store_amazon_books_etl():
         def enrich_book_data(standardised_books: pd.DataFrame):
 
             # Transform the data
-            transformed_books = standardised_books
+            copy = standardised_books.copy()
+            enriched_data = pd.DataFrame()
 
             # check data validity
-            if transformed_books.empty:
+            if copy.empty:
                 raise ValueError('No books found')
 
-            return transformed_books.to_dict(orient='records')
-        
-        return enrich_book_data(standardise_book_data(books))
 
-    @task(task_id="load_book_data")
-    def load_book_data(transformed_books: dict):
+            enriched_data["Author"] = copy["Author"].unique()
+
+            # Average rating of Author = mean(Rating)
+            enriched_data["Average rating of Author"] = copy.groupby("Author")["Rating"].mean()
+            enriched_data["Average rating of Author"].fillna(0, inplace=True) # fill NaN values with 0
+            
+            # Average price of Author = mean(Price)
+            enriched_data["Average price of Author"] = copy.groupby("Author")["Price"].mean()
+            enriched_data["Average price of Author"].fillna(0, inplace=True) # fill NaN values with 0
+
+            # Average Paperback price = mean(Price) where book_type = Paperback
+            enriched_data["Average Paperback price"] = copy[copy["book_type"] == "Paperback"]["Price"].mean()
+            enriched_data["Average Paperback price"].fillna(0, inplace=True) # fill NaN values with 0
+
+            # Average Hardcover price = mean(Price) where book_type = Hardcover
+            enriched_data["Average Hardcover price"] = copy[copy["book_type"] == "Hardcover"]["Price"].mean()
+            enriched_data["Average Hardcover price"].fillna(0, inplace=True) # fill NaN values with 0
+
+            return enriched_data.to_dict(orient='records')
+        
+        standardised_book_data = standardise_book_data(books)
+        return (enrich_book_data(standardised_book_data), standardised_book_data)
+    
+    @task(task_id="load_enriched_data")
+    def load_enriched_data(enriched_metrics: dict):
 
         # check data validity
-        if not transformed_books:
+        if not enriched_metrics:
             raise ValueError('No books found')
-        
+
+        # Load the data via the hook
+        postgres_hook = PostgresHook(postgres_conn_id='books_connection')
+        insertion_query = """
+        INSERT INTO enriched_metrics (author, avg_rating, avg_price, avg_paperback_price, avg_hardcover_price)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+
+        for entry in enriched_metrics:
+            postgres_hook.run(insertion_query, parameters=(entry["Author"], entry["Average rating of Author"], entry["Average price of Author"], entry["Average Paperback price"], entry["Average Hardcover price"]))
+
+        pass
+
+    @task(task_id="load_book_data")
+    def load_book_data(transformed_books: pd.DataFrame):
+
+        # check data validity
+        if transformed_books.empty:
+            raise ValueError('No books found')
+
+        transformed_dict = transformed_books.to_dict(orient='records')
+
         # Load the data via the hook
         postgres_hook = PostgresHook(postgres_conn_id='books_connection')
         insertion_query = """
@@ -167,14 +218,15 @@ def fetch_and_store_amazon_books_etl():
         VALUES (%s, %s, %s, %s, %s, %s)
         """
 
-        for book in transformed_books:
+        for book in transformed_dict:
             postgres_hook.run(insertion_query, parameters=(book["Title"], book["Author"], book["Price"], book["book_type"] ,book["Rating"], book["Rating_count"]))
 
         pass
 
     create_sql_tables()
     raw_book_data = extract_book_data()
-    transformed_book_data = book_data_transformations(raw_book_data)
-    load_book_data(transformed_book_data)
+    enriched_data, standardised_book_data = book_data_transformations(raw_book_data)
+    load_book_data(standardised_book_data)
+    load_enriched_data(enriched_data)
 
 fetch_and_store_amazon_books_dag = fetch_and_store_amazon_books_etl()
