f4f0a583a3269aef34fa2fc960f3f2b83c84ad97
Ryan Tan <ryantanzr.work@gmail.com>
2025-01-10 22:48:58 +0800

    Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -14,7 +14,7 @@ default_args = {
     'owner': 'airflow',
     'start_date': datetime(2025, 1, 10),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
 }
 
 headers = {
